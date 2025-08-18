6a5f3e7890f83c3cdbba821331a6c1e8b168373f
Mickey Beurskens <mickeybeurskens@gmail.com>
2025-08-18 10:43:17 +0200

    Remove unused docker related files

--- build_docker_image.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/sh
-
-# Build docker image from the Dockerfile
-docker.exe build -t stivendias/dmml .
-
-# Upload docker image to DockerHub (requires a configured account)
-docker.exe push stivendias/dmml:latest
